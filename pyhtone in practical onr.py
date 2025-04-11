44d49866c69f78c2e57bea311b43c1ed1c6778f1
vivekzip <122891990+vivekzip@users.noreply.github.com>
2025-04-12 03:52:55 +0530

    Delete pyhtone in practical onr.py

--- pyhtone in practical onr.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from matplotlib import pyplot as plt
-plt.rcParams["figure.figsize"] = [8.00, 4.50]
-plt.rcParams["figure.autolayout"] = True
-columns = ["Item_Name", "Qty_sold"]
-df = pd.read_csv("Sales.csv",usecols=columns)
-print("Contents in csv file:", df)
-plt.plot(df.Item_Name, df.Qty_sold)
-plt.show()
